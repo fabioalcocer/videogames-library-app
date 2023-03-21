e5372561053df84d03811d2e62d5dafd248238cc
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-03-21 17:08:54 -0400

    fix click outside function

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { BiSearch } from "react-icons/bi";
 import { useGameStore } from "@/store/games";
 import { useSetSearchData } from "@/hooks/useGames";
@@ -8,8 +8,20 @@ import SearchResults from "./SearchResults";
 
 function SearchBar() {
   const [query, setQuery] = useState("");
+  const [showSearchResults, setShowSearchResults] = useState(false);
   const { data, isLoading } = useSetSearchData(query);
+  const refSearchResults = useRef<HTMLInputElement>(null);
   const setGames = useGameStore((state) => state.setGames);
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside, true);
+  });
+
+  const handleClickOutside = (e: MouseEvent) => {
+    !refSearchResults.current?.contains(e.target as Node)
+      ? setShowSearchResults(false)
+      : setShowSearchResults(true);
+  };
 
   const handleChange = debounce(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -34,18 +46,23 @@ function SearchBar() {
       <input
         onChange={handleChange}
         onKeyUp={handleKeyPress}
+        ref={refSearchResults}
         type="search"
         name="search"
         className="peer w-[95%] rounded-full border-none bg-zinc-700/60 py-2 pl-11 pr-0 text-xs font-medium text-white transition-all duration-300 placeholder:text-neutral-400 hover:bg-zinc-100 hover:text-zinc-900 focus:border-transparent focus:bg-zinc-100 focus:text-zinc-900 focus:outline-none focus:ring-0 sm:pr-2 sm:text-sm md:py-3 lg:w-full"
         placeholder="Search 884,902 games"
       />
       <BiSearch className="absolute top-2 left-4 fill-zinc-400 text-xl peer-focus:fill-zinc-900 md:top-3" />
-      {isLoading ? (
-        <div className="absolute top-10 z-50 mt-3 flex w-full flex-col gap-2 rounded-xl bg-zinc-800 font-semibold text-zinc-50">
-          <h1>LOADING....</h1>
-        </div>
-      ) : (
-        <SearchResults data={data} />
+      {showSearchResults && (
+        <>
+          {isLoading ? (
+            <div className="absolute top-10 z-50 mt-3 flex w-full flex-col gap-2 rounded-xl bg-zinc-800 font-semibold text-zinc-50">
+              <h1>Searching....</h1>
+            </div>
+          ) : (
+            <SearchResults data={data} />
+          )}
+        </>
       )}
     </div>
   );
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
-import { useEffect, useRef, useState } from "react";
 import type { Result } from "../types/games";
 
 type Props = {
@@ -8,44 +7,30 @@ type Props = {
 };
 
 function SearchResults({ data }: Props) {
-  const [show, setShow] = useState(false);
-  useEffect(() => {
-    document.addEventListener("click", handleClickOutside, true);
-  });
-
-  const refSearchResults = useRef<HTMLDivElement>(null);
-
-  const handleClickOutside = (e: MouseEvent) => {
-    !refSearchResults.current?.contains(e.target as Node)
-      ? setShow(true)
-      : setShow(false);
-  };
-
   return (
-    <div
-      ref={refSearchResults}
-      className={`${
-        show ? "hidden" : "visible"
-      } absolute top-10 z-50 mt-3 flex w-full flex-col gap-2 rounded-xl bg-zinc-800 px-5 pb-4 font-semibold text-zinc-50`}
-    >
-      <h3 className="py-2 text-xl font-bold">Games</h3>
+    <>
+      {data?.length && (
+        <div className="absolute top-10 z-50 mt-3 flex w-full flex-col gap-2 rounded-xl bg-zinc-800 px-5 pb-4 font-semibold text-zinc-50">
+          <h3 className="py-2 text-xl font-bold">Games</h3>
 
-      {data?.slice(0, 4).map((game) => (
-        <div
-          key={game.id}
-          className="flex items-center overflow-hidden rounded-lg hover:bg-stone-900"
-        >
-          <img
-            src={game.background_image}
-            alt="game image"
-            className="h-20 w-14 object-cover"
-          />
-          <p className="cursor-pointer border-b border-gray-700 py-3 px-4 last:border-0">
-            {game.name}
-          </p>
+          {data?.slice(0, 4).map((game) => (
+            <div
+              key={game.id}
+              className="flex items-center overflow-hidden rounded-lg hover:bg-stone-900"
+            >
+              <img
+                src={game.background_image}
+                alt="game image"
+                className="h-20 w-14 object-cover"
+              />
+              <p className="cursor-pointer border-b border-gray-700 py-3 px-4 last:border-0">
+                {game.name}
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   );
 }
 
